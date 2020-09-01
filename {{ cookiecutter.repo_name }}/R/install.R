df73b6b506131fd16156dbb18dae62d7c1078813
Alex <jekyllo@microsoft.com>
2020-09-01 11:13:30 -0700

    don't reinstall conda packages on install recipe

--- {{ cookiecutter.repo_name }}/R/install.R
+++ {{ cookiecutter.repo_name }}/R/install.R
@@ -2,9 +2,11 @@
 
 packages <- c("AzureKusto", "AzureStor")
 
+cat("Installing the R packages from CRAN listed in R/install.R ...\n")
 lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
     install.packages(x, dependencies = TRUE)
     library(x, character.only = TRUE)
   }
 })
+cat("Done.\n")
